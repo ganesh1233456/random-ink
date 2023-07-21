a27c585b325c013cedc425595a16be62471a09a2
Ganesh oli <ganesh@thaiofficefurniture.com>
2023-07-21 17:51:38 +0545

    update

--- lib.rs
+++ lib.rs
@@ -10,22 +10,21 @@ pub mod my_psp34 {
     #[ink(storage)]
     #[derive(Default, Storage)]
     pub struct Contract {
-        random_number: u64,
-        salt: u64,
-        max_value: u64,
+        random_number: u128,
+        salt: u128,
+        max_value: u128,
     }
-
 
     impl Contract {
         #[ink(constructor)]
-        pub fn new(max_value: u64) -> Self {
+        pub fn new(max_value: u128) -> Self {
             let mut instance = Self::default();
             instance.max_value = max_value;
             instance
         }
 
         #[ink(message)]
-        pub fn generate_random_number(&mut self) -> u64 {
+        pub fn generate_random_number(&mut self) -> u128 {
             let seed = self.env().block_timestamp();
             let mut input: Vec<u8> = Vec::new();
             input.extend_from_slice(&seed.to_be_bytes());
@@ -33,18 +32,18 @@ pub mod my_psp34 {
             let mut output = <hash::Keccak256 as hash::HashOutput>::Type::default();
             ink::env::hash_bytes::<hash::Keccak256>(&input, &mut output);
             self.salt += 1;
-            let number = output[0] as u64 % (self.max_value + 1);
+            let number = output[0] as u128 % (self.max_value + 1);
             self.random_number = number;
             number
         }
 
         #[ink(message)]
-        pub fn get_random_number(&self) -> u64 {
+        pub fn get_random_number(&self) -> u128 {
             self.random_number
         }
 
         #[ink(message)]
-        pub fn get_max_value(&self) -> u64 {
+        pub fn get_max_value(&self) -> u128 {
             self.max_value
         }
     }
@@ -55,7 +54,7 @@ pub mod my_psp34 {
 
         #[test]
         fn test_get_pseudo_random() {
-            let max_value: u64 = 15000;
+            let max_value: u128 = 15000;
             let mut contract = Contract::new(max_value);
             let result = contract.generate_random_number();
             assert!(result <= max_value);
